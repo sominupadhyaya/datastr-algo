5818602cc8829571f250e76044c6df31fc76e014
Somin <sominupadhyaya@gmail.com>
2022-07-23 22:01:07 +0545

    complete find duplicate example

--- big-o/findduplicate.py
+++ big-o/findduplicate.py
@@ -1,1 +1,8 @@
-numbers = [3,6,2,3,4,5,3,9,8]
+#example of O(n^2)
+numbers = [3,6,2,3,4,6,3,9,8]
+
+
+for i in range(len(numbers)):
+    for j in range(i+1, len(numbers)):
+        if(numbers[i] == numbers[j]):
+            print(f"{numbers[i]} is duplicate")
