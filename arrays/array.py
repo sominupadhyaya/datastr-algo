ffec8cb31b41c27b200bb5de5fab046590dadbfe
Somin <sominupadhyaya@gmail.com>
2022-08-01 12:05:10 +0545

    complete finding the day scenario

--- arrays/array.py
+++ arrays/array.py
@@ -1,5 +1,13 @@
-stock_prices = [200 , 300 , 340 , 230 , 320]
+stock_prices = [200 , 300 , 340,230 ,320]
 
 print(f"The price on day1 ={ stock_prices[0]}")
 
+# to search the day  for price of 300
 
+for i in range(len(stock_prices)):
+    if(stock_prices[i] == 300):
+         day = i+1
+
+
+
+print(f"The day was : {day}")
