3c60e23ab4cf27e4747a09c3757b6129ac1c8a5d
Somin <sominupadhyaya@gmail.com>
2022-08-02 11:08:42 +0545

    complete array exercise 2

--- arrays/exercise2.py
+++ arrays/exercise2.py
@@ -14,3 +14,38 @@ Using this find out,
 
 
 """
+heros= ['spider man','thor','hulk','iron man','captain america']
+
+#1
+print(heros.__len__()) #O(n)
+
+
+#2
+heros.insert(5,'black panther')
+#or,
+# heros.append('black panther') #O(nheros=['spider man','thor','hulk','iron man','captain america']
+print(heros)
+
+#3 A bit complex way to do this:
+heros.remove('black panther')
+
+for i in range(len(heros)):
+   if (heros[i] == 'hulk'):
+      heros.insert(i+1, 'black panther')
+
+print(heros)
+
+#4
+heros[1:3] = ['doctor strange']
+print(heros)
+
+#5
+heros.sort() #O(n2)
+
+print(heros)
+
+
+
+
+
+
