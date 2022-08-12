962efd70bce04247bcb2bfe798901f060447b14c
Somin <sominupadhyaya@gmail.com>
2022-08-12 10:42:52 +0545

    complete exercise 3

--- arrays/exercise3.py
+++ arrays/exercise3.py
@@ -3,3 +3,18 @@ max number. Max number is something
 you need to take from a user using input() function
 
 """
+
+
+odd_nums = []
+
+max_number = int(input("Enter a max number greater than 2\n"))
+
+print(max_number)
+
+
+for i in range(max_number):
+    if i % 2 != 0:
+        odd_nums.append(i)
+
+print(odd_nums)
+
