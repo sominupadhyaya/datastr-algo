86eed508f5851b39024bda6b6298b35e3bb22d7e
Somin <sominupadhyaya@gmail.com>
2022-08-13 10:10:41 +0545

    complete leetcode question 1

---
+++ leetcode-questions/twosum.js
@@ -0,0 +1,11 @@
+const twoSum = (nums, target) =>{
+    for (let i = 0; i< nums.length; i++){
+        for (let j = i+1; j<nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                return [nums.indexOf(nums[i]) , nums.indexOf(nums[j])]
+            }
+        }
+    }
+}
+
+console.log(twoSum([1,2,3,4] , 3))
