59e7335b812fa8fb3087b366beaf517dfd3cdcfb
Somin <sominupadhyaya@gmail.com>
2022-08-31 20:24:21 +0545

    complete basic case of plusone

---
+++ leetcode-questions/plusone.py
@@ -0,0 +1,15 @@
+
+
+def plusOne(digits):
+    string = ""
+    final_arr = []
+    for i in digits:
+        string += str(i)    
+        res = str(int(string) + 1)
+    
+    for k in res:
+
+        final_arr.append(int(k))
+    
+    print(final_arr)
+plusOne([1,2,3,4,5])
--- leetcode-questions/twosum.js
+++ leetcode-questions/twosum.js
@@ -1,6 +1,6 @@
 const twoSum = (nums, target) =>{
     for (let i = 0; i< nums.length; i++){
-        for (let j = i+1; j<nums.length; j++){
+        for (let j = i; j<nums.length; j++){
             if(nums[i] + nums[j] === target){
                 return [nums.indexOf(nums[i]) , nums.indexOf(nums[j])]
             }
